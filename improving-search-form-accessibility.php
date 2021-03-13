fd047ecc8f9f91cdca34018f235a3d83a8ce2bcf
WebMan Design, Oliver Juhas <webmandesigneu@gmail.com>
2021-03-13 18:02:20 +0100

    1.0.0

---
+++ improving-search-form-accessibility.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Plugin Name:  Improving Search Form Accessibility
+ * Plugin URI:   https://www.webmandesign.eu/portfolio/improving-search-form-accessibility-wordpress-plugin/
+ * Description:  Improves search form accessibility by associating search field label explicitly instead of implicitly.
+ * Version:      1.0.0
+ * Author:       WebMan Design, Oliver Juhas
+ * Author URI:   https://www.webmandesign.eu/
+ * License:      GNU General Public License v3
+ * License URI:  http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:  improving-search-form-accessibility
+ * Domain Path:  /languages
+ *
+ * @copyright  WebMan Design, Oliver Juhas
+ * @license    GPL-3.0, https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @link  https://github.com/webmandesign/improving-search-form-accessibility
+ * @link  https://www.webmandesign.eu
+ *
+ * @link  https://developer.wordpress.org/reference/functions/get_search_form/
+ * @link  https://www.w3.org/WAI/tutorials/forms/labels/#associating-labels-implicitly
+ * @link  https://www.w3.org/WAI/tutorials/forms/labels/#associating-labels-explicitly
+ *
+ * @package  Improving Search Form Accessibility
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+add_filter( 'get_search_form', 'isfa_get_search_form', 0, 2 );
+
+function isfa_get_search_form( $form, $args ) {
+
+	// Variables
+
+		if ( $args['aria_label'] ) {
+			$aria_label = 'aria-label="' . esc_attr( $args['aria_label'] ) . '" ';
+		} else {
+			$aria_label = '';
+		}
+
+		$id = uniqid();
+
+
+	// Output
+
+		return
+			'<form role="search" ' . $aria_label . 'method="get" class="search-form" action="' . esc_url( home_url( '/' ) ) . '">
+				<label for="' . esc_attr( $id ) . '" class="screen-reader-text">' . esc_html_x( 'Search for:', 'label', 'improving-search-form-accessibility' ) . '</label>
+				<input id="' . esc_attr( $id ) . '" type="search" class="search-field" placeholder="' . esc_attr_x( 'Search &hellip;', 'placeholder', 'improving-search-form-accessibility' ) . '" value="' . get_search_query() . '" name="s" />
+				<input type="submit" class="search-submit" value="' . esc_attr_x( 'Search', 'submit button', 'improving-search-form-accessibility' ) . '" />
+			</form>';
+
+}
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Improving Search Form Accessibility ===
+Contributors:      webmandesign
+Donate link:       https://www.webmandesign.eu
+Author URI:        https://www.webmandesign.eu
+Plugin URI:        https://www.webmandesign.eu/portfolio/improving-search-form-accessibility-wordpress-plugin/
+Requires at least: 5.0
+Tested up to:      5.7
+Stable tag:        1.0.0
+License:           GNU General Public License v3
+License URI:       http://www.gnu.org/licenses/gpl-3.0.html
+Tags:              search, form, accessibility
+
+Improves search form accessibility by associating search field label explicitly instead of implicitly.
+
+
+== Description ==
+
+WordPress [search form](https://developer.wordpress.org/reference/functions/get_search_form/) associates [label with search field implicitly](https://www.w3.org/WAI/tutorials/forms/labels/#associating-labels-implicitly). This can cause issues with certain assistive technologies.
+
+This plugin fixes the issue by [associating the label explicitly](https://www.w3.org/WAI/tutorials/forms/labels/#associating-labels-explicitly), thus effectively improving WordPress search form accessibility.
+
+There are no options for the plugin, it works out of the box.
+
+
+== Installation ==
+
+1. Unzip the plugin download file and upload `improving-search-form-accessibility` folder into the `/wp-content/plugins/` directory.
+2. Activate the plugin through the *"Plugins"* menu in WordPress.
+3. Plugin has no options, works immediately after activation.
+
+
+== Changelog ==
+
+Please see the [`changelog.md` file](https://github.com/webmandesign/improving-search-form-accessibility/blob/master/changelog.md) for details.
+
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+Initial release.
